af0f549a4ea9c6f13d3b03ddbfbe2b96f0cf5739
Slavuk <anslava00@mail.ru>
2022-11-18 15:20:46 +1000

    add metrics for logic regression

--- logic_Regression/RegularRegression.py
+++ logic_Regression/RegularRegression.py
@@ -16,8 +16,4 @@ class MyElasticLogisticRegression(MyLogicRegression):
         grad_l2 = 2 * self.l2_coef * self.w
         grad_l2[0] = 0
 
-        assert grad_l1[0] == grad_l2[0] == 0, "Bias в регуляризационные слагаемые не входит!"
-        assert grad_basic.shape == grad_l1.shape == grad_l2.shape == (
-            X_batch.shape[1],), "Градиенты должны быть столбцом из k_features + 1 элементов"
-
         return grad_basic + grad_l1 + grad_l2
